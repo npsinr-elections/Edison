0f14b5a9d0fe51433441ba3b8ac014941281720c
Rishit Bansal <rishit.bansal0@gmail.com>
2015-10-19 21:19:26 +0530

    Added an initial main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask       # Importing Flask
+from flask import render_template
+
+app = Flask(__name__)		# initializing flask app
+
+@app.route('/')
+def home():
+	return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
