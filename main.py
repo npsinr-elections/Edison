a86124de2752b1a2a7c402ad15b71074bc479ec2
Rishit Bansal <rishit.bansal0@gmail.com>
2015-10-26 12:54:36 +0530

    Added candidate route

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask       # Importing Flask
-from flask import render_template
+from flask import Flask,render_template,request       # Importing Flask
+import json
 
 app = Flask(__name__)		# initializing flask app
 
 @app.route('/')
 def home():
 	return render_template("index.html")
+@app.route('/candidates',methods=['GET','POST'])
+def candidate():
+	if request.method == 'GET':
+		jsondata = open("candidates.json").read()
+		return jsondata
+
 
 if __name__ == '__main__':
     app.run(debug=True)
