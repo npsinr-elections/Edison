c6ae7c53997228628ce9faac5c23816f5ae7b3c9
theOriginalMuktaka <muktaka2000@gmail.com>
2016-05-20 16:56:18 +0530

    Fixed candidates.json

--- static/js/FirstInterface.js
+++ static/js/FirstInterface.js
@@ -1,20 +1,79 @@
 /*jslint browser: true*/
-/*globals Poll, Candidate*/
+/*globals Poll*/
 
-function InterfacePoll(givenPollValue) {
+function InterfaceCandidate(givenDumpId, givenCandidateValue, givenCandidate, givenForeColor, givenBackColor) {
 	'use strict';
 
-	var pollValue = givenPollValue,
+	var dumpId = givenDumpId,
+
+		candidateValue = givenCandidateValue,
+		candidate = givenCandidate,
+
+		name = candidateValue.name,
+		image = candidateValue.image,
+		votes = candidateValue.votes = 0,
+
+		foreColor = givenForeColor,
+		backColor = givenBackColor,
+
+		parentElement,
+		candidateButton;
+
+	this.getCandidateButton = function () {
+		return candidateButton;
+	};
+
+	candidateButton = document.createElement('button');
+	candidateButton.appendChild(document.createTextNode(name));
+	candidateButton.addEventListener('click', function () {
+		candidate.vote();
+	});
+
+	parentElement = document.getElementById('container');
+	parentElement.appendChild(candidateButton);
+}
+
+function InterfacePoll(givenDumpId, givenPollValue, givenIndex) {
+	'use strict';
+
+	var dumpId = givenDumpId,
+		id = dumpId.replace(/\s/g, '') + givenIndex,
+
+		pollValue = givenPollValue,
 		poll = new Poll(pollValue.candidates.length),
 
-		interfaceCandidates = [];
+		name = pollValue.name,
+		foreColor = pollValue.foreColor,
+		backColor = pollValue.backColor,
+		candidates = pollValue.candidates,
 
-	function createNewInterfaceCandidate(givenCandidateValue) {
-		console.log(givenCandidateValue.name);
+		interfaceCandidates = [],
+
+		parentElement,
+		headingSlide,
+		electionSlide;
+
+	headingSlide = document.createElement('div');
+
+	electionSlide = document.createElement('div');
+	electionSlide.id = id;
+
+	parentElement = document.getElementById(dumpId);
+	parentElement.appendChild(headingSlide);
+	parentElement.appendChild(electionSlide);
+
+	function createNewInterfaceCandidate(givenCandidateValue, index) {
+		var tempInterfaceCandidate = new InterfaceCandidate(id, givenCandidateValue, pollValue.candidates[index]);
+
+		tempInterfaceCandidate.getCandidateButton().addEventListener('click', function () {
+			poll.addToVotes(index);
+		});
+
+		interfaceCandidates.push(tempInterfaceCandidate);
 	}
 
-	pollValue.candidates.forEach(function (candidate) {
-		createNewInterfaceCandidate(candidate);
+	candidates.forEach(function (candidate, index) {
+		createNewInterfaceCandidate(candidate, index);
 	});
 }
 
@@ -28,8 +87,8 @@ function FirstInterface(givenDumpId) {
 
 		interfacePolls = [];
 
-	function createNewInterfacePoll(pollValue) {
-		var tempInterfacePoll = new InterfacePoll(pollValue);
+	function createNewInterfacePoll(pollValue, index) {
+		var tempInterfacePoll = new InterfacePoll(dumpId, pollValue, index);
 		interfacePolls.push(tempInterfacePoll);
 	}
 
